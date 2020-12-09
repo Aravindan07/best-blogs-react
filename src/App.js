6160193a4622ab00139d84585de19694762deb03
Aravindan07 <50622300+Aravindan07@users.noreply.github.com>
2020-12-09 13:59:59 +0530

    add: Added all the features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import "./App.css";
-import W3schools from "./images/w3schools.png";
-import Medium from "./images/medium.png";
-import Dev from "./images/dev.jpg";
-import CssTricks from "./images/css-tricks.jpg";
-import MDN from "./images/mdn.png";
+import { dummyData } from "./constants.js";
+
+let data = Object.keys(dummyData);
 
 function App() {
+  const [topic, setTopic] = useState("HTML");
+
+  const clickHandler = (item) => {
+    setTopic(item);
+  };
+
   return (
     <div className="wrapper">
       <h1>
@@ -15,108 +20,47 @@ function App() {
         Click on any of the topic below <span>to get the best blogs.</span>
       </p>
       <div className="buttons-wrap">
-        <button>HTML</button>
-        <button>CSS</button>
-        <button>Javascript</button>
-        <button>React</button>
-        <button>Redux</button>
+        {data.map((el) => (
+          <button key={el} onClick={() => clickHandler(el)}>
+            {el}
+          </button>
+        ))}
       </div>
       <div className="card-wrap">
-        <div className="card">
-          <div className="img-holder">
-            <img src={MDN} alt="MDN" />
+        {dummyData[topic].map((el) => (
+          <div className="card" key={el.name}>
+            <div className="img-holder">
+              <img src={el.image} alt={el.name} />
+            </div>
+            <p>{el.name}</p>
+            <p className="low-opacity">
+              {" "}
+              Author :<span> {el.author}</span>
+            </p>
+            <p className="low-opacity">
+              Rating : <span>{el.rating}</span>
+            </p>
+            <a
+              className="link"
+              href={el.link}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Read Now
+            </a>
           </div>
-          <p>HTML: HyperText Markup Language</p>
-          <p className="low-opacity">
-            {" "}
-            Author :<span> MDN</span>
-          </p>
-          <a
-            className="link"
-            href="https://developer.mozilla.org/en-US/docs/Web/HTML"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            Read Now
-          </a>
-        </div>
-
-        <div className="card">
-          <div className="img-holder">
-            <img src={W3schools} alt="w3-schools" />
-          </div>
-          <p>W3Schools is a perfect place to learn HTML.</p>
-          <p className="low-opacity">
-            {" "}
-            Author :<span> W3Schools</span>
-          </p>
-          <a
-            className="link"
-            href="https://www.w3schools.com/html/default.asp"
-            target="_blank"
-            rel="norelation noreferrer"
-          >
-            Read Now
-          </a>
-        </div>
-
-        <div className="card">
-          <div className="img-holder">
-            <img src={CssTricks} alt="CSS-Tricks" />
-          </div>
-          <p>Snippets for HTML</p>
-          <p className="low-opacity">
-            {" "}
-            Author :<span> Chris Coyier</span>
-          </p>
-          <a
-            className="link"
-            href="https://css-tricks.com/snippets/html/"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            Read Now
-          </a>
-        </div>
-
-        <div className="card">
-          <div className="img-holder">
-            <img src={Medium} alt="Medium" />
-          </div>
-          <p>Beginner’s HTML Cheat Sheet.</p>
-          <p className="low-opacity">
-            {" "}
-            Author :<span> Bradley Nice</span>
-          </p>
-          <a
-            className="link"
-            href="https://medium.com/level-up-web/beginners-html-cheat-sheet-f9b1e3ce88a6"
-            target="_blank"
-            rel="norelation noreferrer"
-          >
-            Read Now
-          </a>
-        </div>
-
-        <div className="card">
-          <div className="img-holder">
-            <img src={Dev} alt="Dev.to" />
-          </div>
-          <p>Stop using so many divs! An intro to semantic HTML</p>
-          <p className="low-opacity">
-            {" "}
-            Author :<span> Ken Bellows</span>
-          </p>
-          <a
-            className="link"
-            href="https://dev.to/kenbellows/stop-using-so-many-divs-an-intro-to-semantic-html-3i9i"
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            Read Now
-          </a>
-        </div>
+        ))}
       </div>
+      <p className="low-opacity">
+        Disclaimer:{" "}
+        <span>
+          Above blogs are listed based on my opinion it is not based on any
+          survey or organization.
+        </span>
+      </p>
+      <p className="low-opacity">
+        <span>Made with </span>💚 <span>by</span> Aravind
+      </p>
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,161 @@
+import MDN from "./images/mdn.png";
+import W3Schools from "./images/w3schools.png";
+import CssTricks from "./images/css-tricks.jpg";
+import Medium from "./images/medium.png";
+import Dev from "./images/dev.jpg";
+import FCC from "./images/fcc.jpg";
+import JsInfo from "./images/js-info.png";
+import HackerNoon from "./images/hackernoon.jpeg";
+import ReactImage from "./images/react-img.png";
+
+export const dummyData = {
+  HTML: [
+    {
+      image: MDN,
+      name: "HTML: HyperText Markup Language",
+      author: "MDN",
+      rating: "4.7/5",
+      link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+    },
+    {
+      image: W3Schools,
+      name: "W3Schools is a perfect place to learn HTML.",
+      author: "W3Schools",
+      rating: "4.7/5",
+      link: "https://www.w3schools.com/html/default.asp",
+    },
+    {
+      image: CssTricks,
+      name: "Snippets for HTML",
+      author: "Chris Coyier",
+      rating: "4.5/5",
+      link: "https://css-tricks.com/snippets/html/",
+    },
+    {
+      image: Medium,
+      name: "Beginner’s HTML Cheat Sheet.",
+      author: "Bradley Nice",
+      rating: "4.3/5",
+      link:
+        "https://medium.com/level-up-web/beginners-html-cheat-sheet-f9b1e3ce88a6",
+    },
+    {
+      image: Dev,
+      name: "Stop using so many divs! An intro to semantic HTML",
+      author: "Ken Bellows",
+      rating: "4.3/5",
+      link:
+        "https://dev.to/kenbellows/stop-using-so-many-divs-an-intro-to-semantic-html-3i9i",
+    },
+  ],
+  CSS: [
+    {
+      image: MDN,
+      name: "CSS: Cascading Style Sheets",
+      author: "MDN",
+      rating: "4.7/5",
+      link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+    },
+    {
+      image: W3Schools,
+      name: "W3Schools is a perfect place to learn .",
+      author: "W3Schools",
+      rating: "4.7/5",
+      link: "https://www.w3schools.com/css/default.asp",
+    },
+    {
+      image: CssTricks,
+      name: "Snippets for CSS",
+      author: "Chris Coyier",
+      rating: "4.5/5",
+      link: "https://css-tricks.com/snippets/css/",
+    },
+    {
+      image: FCC,
+      name: "CSS Naming Conventions that Will Save Your Hours",
+      author: "Ohans Emmanuel",
+      rating: "4.5/5",
+      link:
+        "https://medium.com/free-code-camp/css-naming-conventions-that-will-save-you-hours-of-debugging-35cea737d849",
+    },
+    {
+      image: FCC,
+      name: "The 100% correct way to do CSS breakpoints",
+      author: "David Gilbertson",
+      rating: "4.5/5",
+      link:
+        "https://medium.com/free-code-camp/the-100-correct-way-to-do-css-breakpoints-88d6a5ba1862",
+    },
+  ],
+  Javascript: [
+    {
+      image: MDN,
+      name: "JavaScript",
+      author: "MDN",
+      rating: "4.7/5",
+      link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    },
+    {
+      image: JsInfo,
+      name: "The Modern JavaScript Tutorial",
+      author: "JavaScript Info",
+      rating: "4.7/5",
+      link: "https://javascript.info/",
+    },
+    {
+      image: W3Schools,
+      name: "JavaScript Tutorial",
+      author: "W3schools",
+      rating: "4.5/5",
+      link: "https://www.w3schools.com/js/default.asp",
+    },
+    {
+      image: FCC,
+      name: "Is Vanilla JavaScript worth learning? Absolutely.",
+      author: "David Kopal",
+      rating: "4.7/5",
+      link:
+        "https://medium.com/free-code-camp/is-vanilla-javascript-worth-learning-absolutely-c2c67140ac34",
+    },
+    {
+      image: HackerNoon,
+      name: "Javascript Basics For React Beginners",
+      author: "HackerNoon",
+      rating: "4.5/5",
+      link:
+        "https://hackernoon.com/javascript-basics-for-react-beginners-1b1231c2",
+    },
+  ],
+  React: [
+    {
+      image: ReactImage,
+      name: "React Documentation",
+      author: "reactjs.org",
+      rating: "4.5/5",
+      link: "https://reactjs.org/",
+    },
+    {
+      image: Dev,
+      name: "The React Cheatsheet for 2020",
+      author: "Reed Barger",
+      rating: "4.5/5",
+      link:
+        "https://dev.to/reedbarger/the-react-cheatsheet-for-2020-real-world-examples-4hgg",
+    },
+    {
+      image: Medium,
+      name: "Master these five concepts, then master React.",
+      author: "Sacha Greif",
+      rating: "4.5/5",
+      link:
+        "https://medium.com/free-code-camp/the-5-things-you-need-to-know-to-understand-react-a1dbd5d114a3",
+    },
+    {
+      image: ReactImage,
+      name: "How to learn React.js in 2020",
+      author: "Robin Wieruch",
+      rating: "4.5/5",
+      link: "https://www.robinwieruch.de/learn-react-js",
+    },
+  ],
+};
